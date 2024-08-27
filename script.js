64c42aff06627c38f105ae02c61c9bc49cc39b10
filipkk78 <filipkk78@gmail.com>
2024-08-28 00:00:43 +0200

    Add semi-functional UI

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ let gameBoard = {
     row3: ["E", "E", "E"],
 }
 
-function gameController() {
+const startBtn = document.querySelector("#start");
+
+startBtn.addEventListener("click", function () {
     const players = [
         {
             name: "playerX",
@@ -16,89 +18,91 @@ function gameController() {
         }
     ]
 
+    const activePlayerDisplay = document.querySelector("#activePlayer");
     let activePlayer = players[0];
+    activePlayerDisplay.textContent = "Now playing: player X";
+    activePlayerDisplay.style.color = "#0008FF"
 
     const switchPlayerTurn = () => {
         if(activePlayer==players[0]) {
             activePlayer = players[1];
+            activePlayerDisplay.textContent = "Now playing: player O";
+            activePlayerDisplay.style.color = "#FF0000"
         } else if(activePlayer==players[1]) {
             activePlayer = players[0];
+            activePlayerDisplay.textContent = "Now playing: player X";
+            activePlayerDisplay.style.color = "#0008FF"
         }
     }
 
     let destinationTile; 
 
-    function chooseTile() {
-        do {
-        row = prompt("Select the row (1-3)");
-        } while(row!=1 && row!=2 && row!=3);
-        do {
-        column = prompt("Select the column (1-3)");
-        } while(column!=1 && column!=2 && column!=3);
+    function chooseTile(row, column) {
         let selectedRow = row;
         let selectedColumn = column-1;
         destinationTile = gameBoard[`row${selectedRow}`].at(selectedColumn);
         if (destinationTile!="E") {
         console.log(`This tile is already occupied by ${destinationTile}`);
+        switchPlayerTurn()
+        return;
         }
         let modifiedArray = gameBoard[`row${selectedRow}`].with(selectedColumn, activePlayer.symbol);
         gameBoard[`row${selectedRow}`] = modifiedArray;
         return gameBoard;
     }
 
-    
-    
-    function playGame() {
+    const uiBoard = document.querySelector(".board");
+
+    uiBoard.addEventListener("click",(event) => { 
+        chosenRow = event.target.dataset.row;
+        chosenColumn = event.target.dataset.column;
+        if(activePlayer==players[0]) {
+            event.target.classList.add("cross")
+        } else if(activePlayer==players[1]) {
+            event.target.classList.add("circle")
+        }
+        event.target.classList.add()
+        console.log(`Row: ${chosenRow}, Column: ${chosenColumn}`);
+        chooseTile(chosenRow, chosenColumn);
+        console.log(gameBoard.row1);
+        console.log(gameBoard.row2);
+        console.log(gameBoard.row3);
+        console.log("//////////////////");
         let winner = 0;
         function winCheck() {
-            if(
-                gameBoard.row1.at(0)=="X" && gameBoard.row1.at(1)=="X" && gameBoard.row1.at(2)=="X" ||
-                gameBoard.row2.at(0)=="X" && gameBoard.row2.at(1)=="X" && gameBoard.row2.at(2)=="X" ||
-                gameBoard.row3.at(0)=="X" && gameBoard.row3.at(1)=="X" && gameBoard.row3.at(2)=="X" ||
-                gameBoard.row1.at(0)=="X" && gameBoard.row2.at(0)=="X" && gameBoard.row3.at(0)=="X" ||
-                gameBoard.row1.at(1)=="X" && gameBoard.row2.at(1)=="X" && gameBoard.row3.at(1)=="X" ||
-                gameBoard.row1.at(2)=="X" && gameBoard.row2.at(2)=="X" && gameBoard.row3.at(2)=="X" ||
-                gameBoard.row1.at(0)=="X" && gameBoard.row2.at(1)=="X" && gameBoard.row3.at(2)=="X" ||
-                gameBoard.row3.at(0)=="X" && gameBoard.row2.at(1)=="X" && gameBoard.row1.at(2)=="X"
-            ) {
-                winner = players[0].name;
-            }
-            if(
-                gameBoard.row1.at(0)=="O" && gameBoard.row1.at(1)=="O" && gameBoard.row1.at(2)=="O" ||
-                gameBoard.row2.at(0)=="O" && gameBoard.row2.at(1)=="O" && gameBoard.row2.at(2)=="O" ||
-                gameBoard.row3.at(0)=="O" && gameBoard.row3.at(1)=="O" && gameBoard.row3.at(2)=="O" ||
-                gameBoard.row1.at(0)=="O" && gameBoard.row2.at(0)=="O" && gameBoard.row3.at(0)=="O" ||
-                gameBoard.row1.at(1)=="O" && gameBoard.row2.at(1)=="O" && gameBoard.row3.at(1)=="O" ||
-                gameBoard.row1.at(2)=="O" && gameBoard.row2.at(2)=="O" && gameBoard.row3.at(2)=="O" ||
-                gameBoard.row1.at(0)=="O" && gameBoard.row2.at(1)=="O" && gameBoard.row3.at(2)=="O" ||
-                gameBoard.row3.at(0)=="O" && gameBoard.row2.at(1)=="O" && gameBoard.row1.at(2)=="O"
-            ) {
-                winner = players[1].name;
+                if(
+                    gameBoard.row1.at(0)=="X" && gameBoard.row1.at(1)=="X" && gameBoard.row1.at(2)=="X" ||
+                    gameBoard.row2.at(0)=="X" && gameBoard.row2.at(1)=="X" && gameBoard.row2.at(2)=="X" ||
+                    gameBoard.row3.at(0)=="X" && gameBoard.row3.at(1)=="X" && gameBoard.row3.at(2)=="X" ||
+                    gameBoard.row1.at(0)=="X" && gameBoard.row2.at(0)=="X" && gameBoard.row3.at(0)=="X" ||
+                    gameBoard.row1.at(1)=="X" && gameBoard.row2.at(1)=="X" && gameBoard.row3.at(1)=="X" ||
+                    gameBoard.row1.at(2)=="X" && gameBoard.row2.at(2)=="X" && gameBoard.row3.at(2)=="X" ||
+                    gameBoard.row1.at(0)=="X" && gameBoard.row2.at(1)=="X" && gameBoard.row3.at(2)=="X" ||
+                    gameBoard.row3.at(0)=="X" && gameBoard.row2.at(1)=="X" && gameBoard.row1.at(2)=="X"
+                ) {
+                    winner = players[0].name;
+                }
+                if(
+                    gameBoard.row1.at(0)=="O" && gameBoard.row1.at(1)=="O" && gameBoard.row1.at(2)=="O" ||
+                    gameBoard.row2.at(0)=="O" && gameBoard.row2.at(1)=="O" && gameBoard.row2.at(2)=="O" ||
+                    gameBoard.row3.at(0)=="O" && gameBoard.row3.at(1)=="O" && gameBoard.row3.at(2)=="O" ||
+                    gameBoard.row1.at(0)=="O" && gameBoard.row2.at(0)=="O" && gameBoard.row3.at(0)=="O" ||
+                    gameBoard.row1.at(1)=="O" && gameBoard.row2.at(1)=="O" && gameBoard.row3.at(1)=="O" ||
+                    gameBoard.row1.at(2)=="O" && gameBoard.row2.at(2)=="O" && gameBoard.row3.at(2)=="O" ||
+                    gameBoard.row1.at(0)=="O" && gameBoard.row2.at(1)=="O" && gameBoard.row3.at(2)=="O" ||
+                    gameBoard.row3.at(0)=="O" && gameBoard.row2.at(1)=="O" && gameBoard.row1.at(2)=="O"
+                ) {
+                    winner = players[1].name;
+                }
+            if(winner!=0) {
+                console.log(`${winner} wins!`);
             }
         }
-        let row;
-        let column;
-        while(winner==0) {
-            // do {
-            // row = prompt("Select the row (1-3)");
-            // } while(row!=1 && row!=2 && row!=3);
-            // do {
-            // column = prompt("Select the column (1-3)");
-            // } while(column!=1 && column!=2 && column!=3);
-            do {
-                chooseTile();
-            } while(destinationTile!="E")
-            console.log(gameBoard.row1);
-            console.log(gameBoard.row2);
-            console.log(gameBoard.row3);
-            console.log("//////////////////");
-            winCheck();
-            switchPlayerTurn();
-        }
-        return console.log(`${winner} wins!`);
-    }
-    playGame();
-}
+        winCheck();
+        switchPlayerTurn();
+
+    })}, false);
+
 
 function resetBoard() {
     gameBoard = {
@@ -106,4 +110,10 @@ function resetBoard() {
         row2: ["E", "E", "E"],
         row3: ["E", "E", "E"],
     }
+    console.clear();
+    const tiles = Array.from(document.querySelectorAll(".tile"));
+    tiles.forEach((tile) => {
+    tile.classList.remove("circle");
+    tile.classList.remove("cross")
+    })
 }
